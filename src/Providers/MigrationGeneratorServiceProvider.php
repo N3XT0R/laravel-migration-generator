7abfb327212244f5964eea3ea66b0bea3ad8c8e9
Ilya Beliaev <info@php-dev.info>
2020-05-01 14:07:10 +0200

    service provider.

---
+++ src/Providers/MigrationGeneratorServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace N3XT0R\MigrationGenerator\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class MigrationGeneratorServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
