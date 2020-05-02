d78fc66acad21707870795cc1f39efd6a421af24
Ilya Beliaev <info@php-dev.info>
2020-05-02 16:26:20 +0200

    compiler class.

--- src/Service/Generator/Compiler/MigrationCompiler.php
+++ src/Service/Generator/Compiler/MigrationCompiler.php
@@ -3,8 +3,6 @@
 
 namespace N3XT0R\MigrationGenerator\Service\Generator\Compiler;
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
 use N3XT0R\MigrationGenerator\Service\Generator\Definition\Entity\FieldEntity;
 
 class MigrationCompiler
