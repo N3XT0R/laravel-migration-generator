f13b8f87502f43a75a880aa9243fbbf2e9ed495a
Ilya Beliaev <info@php-dev.info>
2020-05-02 16:11:08 +0200

    compiler class.

--- src/Service/Generator/Compiler/MigrationCompiler.php
+++ src/Service/Generator/Compiler/MigrationCompiler.php
@@ -3,10 +3,84 @@
 
 namespace N3XT0R\MigrationGenerator\Service\Generator\Compiler;
 
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use N3XT0R\MigrationGenerator\Service\Generator\Definition\Entity\ResultEntity;
 use Nette\PhpGenerator\ClassType;
+use Nette\PhpGenerator\PhpNamespace;
 use Nette\PhpGenerator\PsrPrinter;
+use Nette\PhpGenerator\Method;
 
 class MigrationCompiler
 {
-    
+
+    protected $class;
+
+
+    public function setNamespace(PhpNamespace $classType): void
+    {
+        $this->class = $classType;
+    }
+
+    public function getNamespace(): PhpNamespace
+    {
+        return $this->class;
+    }
+
+    public function initializeMigration(string $name, string $customMigrationClass = ''): void
+    {
+        $className = Migration::class;
+        if (!empty($customMigrationClass) && class_exists($customMigrationClass)) {
+            $className = $customMigrationClass;
+        }
+
+        $namespace = new PhpNamespace('Database\\Migration');
+        $namespace->addUse($className);
+        $namespace->addUse(Blueprint::class);
+
+        $class = new ClassType($name);
+        $class->addExtend($className);
+        $upMethod = $class->addMethod('up');
+        $upMethod->setPublic();
+        $upMethod->setReturnType('void');
+
+
+        $downMethod = $class->addMethod('down');
+        $downMethod->setPublic();
+        $downMethod->setReturnType('void');
+
+        $namespace->add($class);
+
+        $this->setNamespace($namespace);
+    }
+
+    public function createByResult(ResultEntity $entity): void
+    {
+        $namespace = $this->getNamespace();
+        $class = $namespace->getClasses()[0];
+
+        $this->addCreateTable($class->getMethod('up'), $entity->getResultByKey('table'));
+    }
+
+
+    protected function addCreateTable(Method $method, array $data): void
+    {
+        $body = '
+            Schema::table();
+        ';
+
+        $method->setBody($body);
+    }
+
+
+    public function getContent(): string
+    {
+        $printer = new PsrPrinter();
+        $class = $this->getNamespace();
+
+        $content = $printer->printNamespace($class);
+        $content = str_replace('namespace Database\Migration;', '<?php', $content);
+
+        return $content;
+    }
 }
---
+++ tests/Unit/Service/Generator/Compiler/MigrationCompilerTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Tests\Unit\Service\Generator\Compiler;
+
+use N3XT0R\MigrationGenerator\Service\Generator\Definition\Entity\FieldEntity;
+use N3XT0R\MigrationGenerator\Service\Generator\Definition\Entity\ResultEntity;
+use Tests\TestCase;
+use N3XT0R\MigrationGenerator\Service\Generator\Compiler\MigrationCompiler;
+
+class MigrationCompilerTest extends TestCase
+{
+    protected $compiler;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->compiler = new MigrationCompiler();
+    }
+
+    public function testCreateMigrationClass(): void
+    {
+        $field = new FieldEntity();
+        $field->setColumnName('id');
+        $field->setType('bigInteger');
+        $field->setComment('fuck you');
+        $field->setTable('da_attributevalue');
+        $field->setArguments(['default' => 1, 'nullable' => 1, 'autoIncrement' => 1]);
+        $resultEntity = new ResultEntity();
+        $resultEntity->setResultByKey('table', ['da_attributevalue' => [$field]]);
+        $this->compiler->initializeMigration('CreateTableBla');
+        var_dump($this->compiler->getContent());
+    }
+}
