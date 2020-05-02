e7706bd72cc9ba0ed442dad0d5a94f4a3f408946
Ilya Beliaev <info@php-dev.info>
2020-05-02 15:26:22 +0200

    compiler class.

---
+++ src/Service/Generator/Compiler/MigrationCompiler.php
@@ -0,0 +1,12 @@
+<?php
+
+
+namespace N3XT0R\MigrationGenerator\Service\Generator\Compiler;
+
+use Nette\PhpGenerator\ClassType;
+use Nette\PhpGenerator\PsrPrinter;
+
+class MigrationCompiler
+{
+    
+}
