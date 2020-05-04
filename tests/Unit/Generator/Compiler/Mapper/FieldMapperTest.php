7eeaecb958e18d5041361ab54e59d17ae7035595
Ilya Beliaev <info@php-dev.info>
2020-05-04 16:13:38 +0200

    tests

---
+++ tests/Unit/Generator/Compiler/Mapper/FieldMapperTest.php
@@ -0,0 +1,38 @@
+<?php
+
+
+namespace Tests\Unit\Generator\Compiler\Mapper;
+
+
+use N3XT0R\MigrationGenerator\Service\Generator\Compiler\Mapper\FieldMapper;
+use N3XT0R\MigrationGenerator\Service\Generator\Definition\Entity\FieldEntity;
+use Tests\TestCase;
+
+class FieldMapperTest extends TestCase
+{
+    protected $mapper;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->mapper = new FieldMapper();
+    }
+
+    public function testMapWorksOnlyWithFieldEntities(): void
+    {
+        $this->assertCount(0, $this->mapper->map([1, 2, 3]));
+    }
+
+    public function testMapWorksWithoutArgumentsAndWithoutOptions(): void
+    {
+        $field = new FieldEntity();
+        $field->setType('bigInteger');
+        $field->setColumnName('id');
+
+        $data = [$field];
+
+        $result = $this->mapper->map($data);
+        $this->assertCount(1, $result);
+        $this->assertStringContainsString("\$table->bigInteger('id');", $result[0]);
+    }
+}
