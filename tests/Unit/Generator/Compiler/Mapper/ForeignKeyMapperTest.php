e06d72e6ef7640622cfd81df49742e82627dea6e
Ilya Beliaev <info@php-dev.info>
2020-05-04 16:39:57 +0200

    tests

--- tests/Unit/Generator/Compiler/Mapper/ForeignKeyMapperTest.php
+++ tests/Unit/Generator/Compiler/Mapper/ForeignKeyMapperTest.php
@@ -5,6 +5,7 @@ namespace Tests\Unit\Generator\Compiler\Mapper;
 
 
 use N3XT0R\MigrationGenerator\Service\Generator\Compiler\Mapper\ForeignKeyMapper;
+use N3XT0R\MigrationGenerator\Service\Generator\Definition\Entity\ForeignKeyEntity;
 use Tests\TestCase;
 
 class ForeignKeyMapperTest extends TestCase
@@ -21,5 +22,21 @@ class ForeignKeyMapperTest extends TestCase
     {
         $data = [1, 2, 3];
         $this->assertCount(0, $this->mapper->map($data));
+    }
+
+    public function testMapWithOneForeignKey(): void
+    {
+        $foreignKey = new ForeignKeyEntity();
+        $foreignKey->setLocalColumn('test');
+        $foreignKey->setLocalTable('table');
+        $foreignKey->setReferencedColumn('id');
+        $foreignKey->setReferencedTable('reference');
+
+        $result = $this->mapper->map([$foreignKey]);
+        $this->assertCount(1, $result);
+        $this->assertStringContainsString(
+            '$table->foreign(\'test\')->references(\'id\')->on(\'reference\');',
+            $result[0]
+        );
     }
 }
