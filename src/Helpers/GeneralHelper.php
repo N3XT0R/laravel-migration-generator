ae03e32e57a55a230cbbbfe4c809cc64e7e09617
Ilya Beliaev <info@php-dev.info>
2020-05-06 09:33:57 +0200

    changed possible versions.

---
+++ src/Helpers/GeneralHelper.php
@@ -0,0 +1,14 @@
+<?php
+
+if (!function_exists('config_path')) {
+    /**
+     * Get the configuration path.
+     *
+     * @param string $path
+     * @return string
+     */
+    function config_path(string $path = ''): string
+    {
+        return app()->basePath() . '/config' . ($path ? '/' . $path : $path);
+    }
+}
