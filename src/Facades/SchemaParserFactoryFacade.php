b676094fdc9888f532f999f40dd1d7aebed6f721
Ilya Beliaev <info@php-dev.info>
2025-06-15 14:39:14 +0200

    moved mysql to drivers directory, added support for multiple dbms.

--- src/Facades/SchemaParserFactoryFacade.php
+++ src/Facades/SchemaParserFactoryFacade.php
@@ -2,9 +2,14 @@
 
 namespace N3XT0R\MigrationGenerator\Facades;
 
+use Illuminate\Database\ConnectionInterface;
 use Illuminate\Support\Facades\Facade;
 use N3XT0R\MigrationGenerator\Service\Parser\SchemaParserFactoryInterface;
+use N3XT0R\MigrationGenerator\Service\Parser\SchemaParserInterface;
 
+/**
+ * @method static SchemaParserInterface create(ConnectionInterface $connection)
+ */
 class SchemaParserFactoryFacade extends Facade
 {
     protected static function getFacadeAccessor(): string
