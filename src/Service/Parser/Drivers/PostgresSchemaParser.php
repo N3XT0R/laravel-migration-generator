ddfac3d59066d0bc1c2433d5adcedd99871c763d
Ilya Beliaev <info@php-dev.info>
2025-06-17 02:56:16 +0200

    fixed classes.

--- src/Service/Parser/Drivers/PostgresSchemaParser.php
+++ src/Service/Parser/Drivers/PostgresSchemaParser.php
@@ -36,7 +36,7 @@ class PostgresSchemaParser extends AbstractSchemaParser
 
     protected function getForeignKeyConstraints(string $schema, string $tableName): array
     {
-        return $this->getConnection()->select(
+        $result = $this->getConnection()->select(
             '
             SELECT
                 tc.constraint_name AS name
@@ -44,38 +44,34 @@ class PostgresSchemaParser extends AbstractSchemaParser
                 information_schema.table_constraints tc
             WHERE
                 tc.constraint_type = \'FOREIGN KEY\'
-                AND tc.table_schema = ?
-                AND tc.table_name = ?
                 AND tc.table_catalog = ?
+                AND tc.table_name = ?
+                AND tc.table_schema = ?
             ',
             [$schema, $tableName, 'public']
         );
+        return $result;
     }
 
     protected function getRefNameByConstraintName(string $schema, string $constraintName): string
     {
         $conn = $this->getConnection();
-        /**
-         * TODO:
-         * Implement an additional parameter to allow selection of the `table_catalog` (database name).
-         * Currently, the query only filters by `table_schema`.
-         * For multi-database scenarios, passing the catalog dynamically is necessary
-         * to enable querying tables across different databases accurately.
-         */
-        $r = $conn->selectOne("
-            SELECT
-                kcu.referenced_table_name AS refName
-            FROM
-                information_schema.referential_constraints rc
-                JOIN information_schema.key_column_usage kcu
-                    ON rc.constraint_name = kcu.constraint_name
-                    AND rc.constraint_schema = kcu.constraint_schema
-            WHERE
-                rc.constraint_schema = ?
-                AND rc.constraint_name = ?
-            LIMIT 1
-        ", [$schema, $constraintName]);
 
-        return $r?->refName ?? '';
+        $result = $conn->selectOne("
+        SELECT
+            c_rel.relname AS refName
+        FROM
+            pg_constraint con
+            JOIN pg_class tbl ON con.conrelid = tbl.oid
+            JOIN pg_class c_rel ON con.confrelid = c_rel.oid
+            JOIN pg_namespace ns ON ns.oid = tbl.relnamespace
+        WHERE
+            con.contype = 'f' -- foreign key constraint
+            AND ns.nspname = ?
+            AND con.conname = ?
+        LIMIT 1
+    ", [$schema, $constraintName]);
+
+        return $result?->refName ?? '';
     }
 }
