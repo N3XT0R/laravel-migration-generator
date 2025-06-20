ff14999d09fda0d7951ea27f8c41f2d629bf300e
Ilya Beliaev <info@php-dev.info>
2025-06-20 22:05:59 +0200

    mssql driver fixed.

--- src/Service/Parser/Drivers/MSSQLSchemaParser.php
+++ src/Service/Parser/Drivers/MSSQLSchemaParser.php
@@ -9,9 +9,8 @@ class MSSQLSchemaParser extends AbstractSchemaParser
     public function getTablesFromSchema(string $schema): array
     {
         $tables = [];
-        // MSSQL behandelt Schema-Namen (z.B. dbo)
         $queryResult = $this->getConnection()->select(
-            'SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = ? AND TABLE_TYPE = ? AND TABLE_NAME != ?',
+            'SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_CATALOG = ? AND TABLE_TYPE = ? AND TABLE_NAME != ?',
             [$schema, 'BASE TABLE', 'migrations']
         );
 
@@ -24,7 +23,6 @@ class MSSQLSchemaParser extends AbstractSchemaParser
 
     protected function getForeignKeyConstraints(string $schema, string $tableName): array
     {
-        // MSSQL verwendet INFORMATION_SCHEMA.TABLE_CONSTRAINTS
         return $this->getConnection()->select(
             "
             SELECT
@@ -33,7 +31,7 @@ class MSSQLSchemaParser extends AbstractSchemaParser
                 INFORMATION_SCHEMA.TABLE_CONSTRAINTS
             WHERE
                 CONSTRAINT_TYPE = 'FOREIGN KEY'
-                AND TABLE_SCHEMA = ?
+                AND TABLE_CATALOG = ?
                 AND TABLE_NAME = ?
             ",
             [$schema, $tableName]
@@ -42,12 +40,11 @@ class MSSQLSchemaParser extends AbstractSchemaParser
 
     protected function getRefNameByConstraintName(string $schema, string $constraintName): string
     {
-        // In MSSQL kann man referenzierte Tabelle über INFORMATION_SCHEMA.REFERENTIAL_CONSTRAINTS und KEY_COLUMN_USAGE abfragen
         $conn = $this->getConnection();
 
         $r = $conn->selectOne("
             SELECT
-                kcu2.TABLE_NAME AS refName
+                kcu2.TABLE_NAME AS refname
             FROM
                 INFORMATION_SCHEMA.REFERENTIAL_CONSTRAINTS rc
                 INNER JOIN INFORMATION_SCHEMA.KEY_COLUMN_USAGE kcu1
@@ -55,12 +52,12 @@ class MSSQLSchemaParser extends AbstractSchemaParser
                 INNER JOIN INFORMATION_SCHEMA.KEY_COLUMN_USAGE kcu2
                     ON rc.UNIQUE_CONSTRAINT_NAME = kcu2.CONSTRAINT_NAME
             WHERE
-                kcu1.TABLE_SCHEMA = ?
+                kcu1.TABLE_CATALOG = ?
                 AND kcu1.CONSTRAINT_NAME = ?
             ",
             [$schema, $constraintName]
         );
 
-        return $r?->refName ?? '';
+        return $r?->refname ?? '';
     }
 }
