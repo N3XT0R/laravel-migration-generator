ab2a101ebb94d89a1a65dad179e84cd8b68c925d
Ilya Beliaev <info@php-dev.info>
2025-06-21 00:45:07 +0200

    docs added.

---
+++ docs/conf.py
@@ -0,0 +1,17 @@
+# docs/conf.py
+
+project = 'Laravel Migration Generator'
+author = 'N3XT0R'
+release = '8.0.0'
+
+extensions = [
+    'myst_parser',
+]
+
+source_suffix = {
+    '.md': 'markdown',
+}
+
+master_doc = 'index'
+exclude_patterns = []
+html_theme = 'furo'
---
+++ docs/index.md
@@ -0,0 +1,10 @@
+# Laravel Migration Generator
+
+Welcome to the documentation for Laravel Migration Generator.
+
+## Contents
+
+- [Installation](installation.md)
+- [Usage](usage.md)
+- [Migrations](migrations.md)
+- [Architecture](architecture.md)
