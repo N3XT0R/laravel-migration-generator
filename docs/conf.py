257ac67cf6148d5ee3458f8cda66ea4dbe03c940
Ilya Beliaev <info@php-dev.info>
2025-06-21 00:51:18 +0200

    docs added.

--- docs/conf.py
+++ docs/conf.py
@@ -4,6 +4,8 @@ project = 'Laravel Migration Generator'
 author = 'N3XT0R'
 release = '8.0.0'
 
+version = release
+
 extensions = [
     'myst_parser',
 ]
@@ -15,3 +17,17 @@ source_suffix = {
 master_doc = 'index'
 exclude_patterns = []
 html_theme = 'furo'
+
+import os
+if os.environ.get('READTHEDOCS') == 'True':
+    github_user = 'N3XT0R'
+    github_repo = 'laravel-migration-generator'
+    github_version = 'master'
+
+    html_context = {
+        'display_github': True,
+        'github_user': github_user,
+        'github_repo': github_repo,
+        'github_version': github_version,
+        'conf_py_path': '/docs/',
+    }
