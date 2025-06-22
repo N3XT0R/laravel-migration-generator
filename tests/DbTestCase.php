628227db6a073b3b12bb884cd171b115264c7a3d
Ilya Beliaev <info@php-dev.info>
2025-06-23 01:43:30 +0200

    fixed generation of indexes.

--- tests/DbTestCase.php
+++ tests/DbTestCase.php
@@ -4,8 +4,12 @@
 namespace Tests;
 
 
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
 class DbTestCase extends TestCase
 {
+
+    use RefreshDatabase;
 
 
     protected array $migrations = [
