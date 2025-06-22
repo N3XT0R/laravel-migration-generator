7ea0eedcd6c7274bcd521173ddbaaba73802a60b
Ilya Beliaev <info@php-dev.info>
2025-06-23 01:47:50 +0200

    fixed generation of indexes.

--- src/Config/migration-generator.php
+++ src/Config/migration-generator.php
@@ -2,17 +2,12 @@
 
 use N3XT0R\MigrationGenerator\Service\Generator\Compiler\Mapper;
 use N3XT0R\MigrationGenerator\Service\Generator\Definition;
-use N3XT0R\MigrationGenerator\Service\Generator\Normalization\Processors\PivotProcessor;
 
 return [
     'definitions' => [
         'table' => [
             'class' => Definition\TableDefinition::class,
             'requires' => [],
-        ],
-        'primaryKey' => [
-            'class' => Definition\PrimaryKeyDefinition::class,
-            'requires' => ['table'],
         ],
         'foreignKey' => [
             'class' => Definition\ForeignKeyDefinition::class,
@@ -28,10 +23,7 @@ return [
             'class' => Mapper\FieldMapper::class,
             'requires' => [],
         ],
-        'primaryKey' => [
-            'class' => Mapper\PrimaryKeyMapper::class,
-            'requires' => ['table'],
-        ],
+
         'foreignKey' => [
             'class' => Mapper\ForeignKeyMapper::class,
             'requires' => ['table'],
@@ -41,10 +33,5 @@ return [
             'requires' => ['table', 'foreignKey'],
         ],
     ],
-    'normalizer' => [
-        'pivot' => [
-            'class' => PivotProcessor::class,
-            'requires' => ['primary', 'foreignKey', 'table'],
-        ],
-    ],
+
 ];
