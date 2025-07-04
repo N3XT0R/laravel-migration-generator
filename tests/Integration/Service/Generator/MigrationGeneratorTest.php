305d9a66a12264e05475bc2478bb1bb6e2f78231
Ilya Beliaev <info@php-dev.info>
2025-07-04 22:31:07 +0200

    tests added.

--- tests/Integration/Service/Generator/MigrationGeneratorTest.php
+++ tests/Integration/Service/Generator/MigrationGeneratorTest.php
@@ -20,21 +20,31 @@ class MigrationGeneratorTest extends DbTestCase
         );
     }
 
+
+    public function testMakeGeneratorWithoutConnectionNameThrowsException(): void
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage('missing key connectionName in params.');
+        $this->app->make(
+            MigrationGeneratorInterface::class
+        );
+    }
+
     public function testGenerateMigrationForTable(): void
     {
-        $path = $this->resourceFolder . 'ExpectedMigrations/';
+        $path = $this->resourceFolder.'ExpectedMigrations/';
         $this->generator->setMigrationDir($path);
         $result = $this->generator->generateMigrationForTable('testing', 'fields_test');
         $this->assertTrue($result);
         $files = $this->generator->getMigrationFiles();
         foreach ($files as $file) {
-            $this->assertFileExists($path . DIRECTORY_SEPARATOR . $file);
+            $this->assertFileExists($path.DIRECTORY_SEPARATOR.$file);
         }
     }
 
     public function testGenerateMigrationForTableWithUnknownTableReturnsFalse(): void
     {
-        $path = $this->resourceFolder . 'ExpectedMigrations/';
+        $path = $this->resourceFolder.'ExpectedMigrations/';
         $this->generator->setMigrationDir($path);
         $result = $this->generator->generateMigrationForTable('testing', uniqid('test', true));
         $this->assertFalse($result);
@@ -43,7 +53,7 @@ class MigrationGeneratorTest extends DbTestCase
 
     public function tearDown(): void
     {
-        $files = glob($this->resourceFolder . 'ExpectedMigrations/*');
+        $files = glob($this->resourceFolder.'ExpectedMigrations/*');
         foreach ($files as $file) {
             if (is_file($file)) {
                 unlink($file);
